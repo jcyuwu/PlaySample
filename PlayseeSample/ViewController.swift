335abcce957262a8ea678c060147d58188a806b0
Kyle Jhong <jcyu256@gmail.com>
2024-04-11 20:15:27 +0800

    Initial Commit

---
+++ PlayseeSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayseeSample
+//
+//  Created by jcyuwu on 2024/4/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
